616d5a5685cda9c64ab076c27b84f59d6926c868
smiley <smiley@chillerlan.net>
2017-11-22 23:10:06 +0100

    :sparkles:

---
+++ src/TraitException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Class TraitException
+ *
+ * @filesource   TraitException.php
+ * @created      13.11.2017
+ * @package      chillerlan\Traits
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2017 Smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\Traits;
+
+class TraitException extends \Exception{}
