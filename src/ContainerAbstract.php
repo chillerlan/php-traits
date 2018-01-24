c540c0fde497e9051b3f368d6c20694b2bf53911
codemasher <smiley@chillerlan.net>
2018-01-24 02:49:38 +0100

    :sparkles:

---
+++ src/ContainerAbstract.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class ContainerAbstract
+ *
+ * @filesource   ContainerAbstract.php
+ * @created      24.01.2018
+ * @package      chillerlan\Traits
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2018 Smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\Traits;
+
+abstract class ContainerAbstract{
+	use Container;
+}
