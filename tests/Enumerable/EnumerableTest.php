cfe7a98ac9d28cce5b2155b2e8e18fa3c78d5a9a
codemasher <smiley@chillerlan.net>
2018-06-22 02:08:00 +0200

    :octocat: +EnumerableTest

--- tests/Enumerable/EnumerableTest.php
+++ tests/Enumerable/EnumerableTest.php
@@ -16,26 +16,100 @@ use PHPUnit\Framework\TestCase;
 
 class EnumerableTest extends TestCase{
 
-	public function testEnumerable(){
-		$arr = str_split('ABC');
+	protected $array = [1, 'two', 3, 'four', 5];
 
-		/** @var \chillerlan\TraitTest\Enumerable\EnumerableContainer $enum */
-		$enum = new EnumerableContainer($arr);
+	/**
+	 * @var \chillerlan\TraitTest\Enumerable\EnumerableContainer
+	 */
+	protected $enumerable;
 
-		$enum->__each(function($v, $k) use ($arr){
-			$this->assertSame($arr[$k], $v);
+	protected function setUp(){
+		$this->enumerable = new EnumerableContainer($this->array);
+	}
+
+	public function testMap(){
+
+		$arr = $this->enumerable
+			->__map(function($v, $k){
+				return $v;
+			})
+		;
+
+		$this->assertSame($arr, $this->enumerable->__toArray());
+	}
+
+	public function testReverse(){
+		$arr = $this->enumerable
+			->__reverse()
+			->__toArray()
+		;
+
+		$this->assertSame(array_reverse($this->array), $arr);
+	}
+
+	public function testEach(){
+
+		$this->enumerable
+			->__each(function($v, $k){
+				$this->assertSame($this->array[$k], $v);
+			})
+		;
+
+	}
+
+	public function testFirst(){
+		$this->assertSame(1, $this->enumerable->__first());
+	}
+
+	public function testLast(){
+		$this->assertSame(5, $this->enumerable->__last());
+	}
+
+	public function testClear(){
+
+		$arr = $this->enumerable
+			->__clear()
+			->__toArray()
+		;
+
+		$this->assertSame([], $arr);
+	}
+
+	public function testInspect(){
+
+		$expected =  'Array
+(
+    [0] => 1
+    [1] => two
+    [2] => 3
+    [3] => four
+    [4] => 5
+)
+';
+
+		$expected = str_replace("\r", '', $expected); // will we ever settle on ONE line ending that is \n???
+
+		$this->assertSame($expected, $this->enumerable->__inspect());
+	}
+
+	public function testFindAll(){
+		$enum = new EnumerableContainer([1, 'two', 3, 'four', 5]);
+
+		$arr = $enum->__findAll(function($e, $i){
+			return is_string($e);
 		});
 
-		$arr2 = $enum->__map(function($v, $k){
-			return $v;
+		$this->assertSame(['two', 'four'], $arr);
+	}
+
+	public function testReject(){
+		$enum = new EnumerableContainer([1, 'two', 3, 'four', 5]);
+
+		$arr = $enum->__reject(function($e, $i){
+			return is_string($e);
 		});
 
-		$this->assertSame(array_values($arr), $arr2);
-		$this->assertSame($arr, $enum->__toArray());
-
-		$arr2 = $enum->__reverse()->__toArray();
-
-		$this->assertSame(array_reverse($arr), $arr2);
+		$this->assertSame([1, 3, 5], $arr);
 	}
 
 	/**
@@ -45,4 +119,21 @@ class EnumerableTest extends TestCase{
 	public function testMapInvalidCallback(){
 		(new EnumerableContainer([]))->__map('foo');
 	}
+
+	/**
+	 * @expectedException \chillerlan\Traits\TraitException
+	 * @expectedExceptionMessage invalid callback
+	 */
+	public function testFindAllInvalidCallback(){
+		(new EnumerableContainer([]))->__findAll('foo');
+	}
+
+	/**
+	 * @expectedException \chillerlan\Traits\TraitException
+	 * @expectedExceptionMessage invalid callback
+	 */
+	public function testRejectInvalidCallback(){
+		(new EnumerableContainer([]))->__reject('foo');
+	}
+
 }
